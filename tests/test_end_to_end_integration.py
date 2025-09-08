765021a8ba2b1b2bbdc7162f108568e7be84b623
explodingdinosaurs <explodingdinosaurs@gmail.com>
2025-09-08 13:39:28 +1000

    Changed config directory name

--- tests/test_end_to_end_integration.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from firm_ce.model import Model
-
-
-@pytest.mark.slow
-def test_end_to_end_integration():
-    model = Model(config_directory="tests/test_24hr_config_data/config")
-    model.solve()
