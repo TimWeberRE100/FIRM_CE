4532820ccfc5f8284da4c36c01c6b0a863fa35d8
explodingdinosaurs <explodingdinosaurs@gmail.com>
2025-09-08 13:54:03 +1000

    tests/test_end_to_end_integration.py

--- tests/test_end_to_end_integration.py
+++ tests/test_end_to_end_integration.py
@@ -4,5 +4,5 @@ from firm_ce.model import Model
 
 @pytest.mark.slow
 def test_end_to_end_integration():
-    model = Model(config_directory="tests/test_24hr_config_data/config")
+    model = Model(config_directory="tests/test_1hr_config_data/config")
     model.solve()
